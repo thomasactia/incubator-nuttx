e06c2f7cfc605b339194d3e82d53280b4e812d31
Thomas Axelsson <thomas.axelsson@actia.se>
2020-02-24 12:38:34 +0000

    arch/arm/src/imxrt/imxrt_usbdev.c: Drop old copyright header in favor of ASF license

--- arch/arm/src/imxrt/imxrt_usbdev.c
+++ arch/arm/src/imxrt/imxrt_usbdev.c
@@ -40,62 +40,6 @@
  ****************************************************************************/
 
 /****************************************************************************
- * arch/arm/src/imxrt/imxrt_usbdev.c
- *
- *
- * TODO: FIX LICENSE. Must be Apache from now on for upstreaming.
- *       Must have permissions from copyright holders.
- *
- *
- *   Copyright (C) 2020 Actia Nordic AB
- *   Authors: Thomas Axelsson <thomas.axelsson@actia.se>
- *            Simon <C3><85>str<C3><B6>m <simon.astrom@actia.se>
- *
- * Mainly an adjustment of the LPC43xx USB driver:
- *
- *   Copyright (C) 2012-2013 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
- *
- * Part of the NuttX OS and based, in part, on the LPC31xx USB driver:
- *
- *   Authors: David Hewson
- *            Gregory Nutt <gnutt@nuttx.org>
- *
- * Which, in turn, was based on the LPC2148 USB driver:
- *
- *   Copyright (C) 2010-2012 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
-/****************************************************************************
  * Included Files
  ****************************************************************************/
 
